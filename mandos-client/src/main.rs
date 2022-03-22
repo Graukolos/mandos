d713f1408ffc4bd241dc619a6af5817ba493fab8
Max Erdelmeier <erdelmei@rhrk.uni-kl.de>
2022-03-22 09:21:46 +0100

    Introduce mandos-lib for shared code

--- mandos-client/src/main.rs
+++ mandos-client/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpStream;
+use ciborium::de::from_reader;
+use mandos_lib::Message;
+
 fn main() {
-    println!("Hello, world!");
+    let stream = TcpStream::connect("127.0.0.1:25565").unwrap();
+
+    let message: Message = from_reader(stream).unwrap();
+
+    println!("{:?}", message);
 }
--- mandos-server/src/main.rs
+++ mandos-server/src/main.rs
@@ -1,3 +1,16 @@
+use std::net::{TcpListener, TcpStream};
+use ciborium::ser::into_writer;
+use mandos_lib::Message;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:25565").expect("Failed to create Listener, is another server already running?");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        server_handle(stream);
+    }
+}
+
+fn server_handle(stream: TcpStream) {
+    into_writer(&Message::Ciao, stream).unwrap();
 }
